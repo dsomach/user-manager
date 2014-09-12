f06fca034aee9c6369b745deec0d910f92066bf3
David Somach <drsomach@gmail.com>
2014-09-12 01:01:58 -0400

    implement basic user list, create user

--- app/api/account_api.rb
+++ app/api/account_api.rb
@@ -4,11 +4,16 @@ class AccountAPI < Grape::API
   get 'count' do
     {:count => User.count}
   end
+  
+  get 'list' do
+    User.all.collect{|u| u.name}
+  end
 
+  #TODO create will be a POST
   get 'create' do
     u = User.new
-    u.name = 'David'
-    u.email = 'drsomach@gmail.com'
+    u.name = params[:name] || 'David'
+    u.email = params[:email] || 'drsomach@gmail.com'
     u.save
     {:user => u.to_json}
   end
